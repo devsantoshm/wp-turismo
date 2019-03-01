c203a88033f902d2ba0f110de8f6f96301cebf35
SantosHM <dosantoshm@gmail.com>
2019-03-01 11:19:55 -0500

    Agregando estilos al menu footer

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 	</div> <!-- #contenido -->
 	<footer class="site-footer">
-		desde el footer
+		<div class="container">
+			<nav id="footer-navigation" class="footer-navigation">
+				<!-- si creamos otro menu, tenemos que especificar el nombre del menu creado -->
+				<?php wp_nav_menu(array('theme_location' => 'menu_principal')); ?>
+			</nav>
+		</div>
+		<!-- limpiar floats con clear -->
+		<div class="clear"></div>
+		<div class="copyright">
+			<p>Visita Toronto <?php echo date('Y'); ?></p>
+		</div>
 	</footer>
 </div> <!-- #page -->
 </body>
