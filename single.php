82e71204cbc4c25e668091ab6a408307700fb3f0
SantosHM <dosantoshm@gmail.com>
2019-03-03 16:05:55 -0500

    Creando el archivo single.php como plantilla para cada entrada

---
+++ single.php
@@ -0,0 +1,23 @@
+
+<?php get_header(); ?>
+
+<?php while(have_posts()): the_post();?>
+
+	<?php if(has_post_thumbnail()): ?>
+		<div class="destacada">
+			<?php the_post_thumbnail('destacada'); // Mostrar imagen seleccionada del post en wp-admin ?>
+			<h2><?php the_title(); ?></h2>
+		</div>
+	<?php else: ?>
+		<h2 class="noimagen"><?php the_title(); ?></h2>
+	<?php endif; ?>
+
+	<div id="primary" class="primary">
+		<?php the_content(); ?>	
+	</div>
+
+<?php endwhile; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
