978bfa8787a6ea590386968453935a98e4ab808b
SantosHM <dosantoshm@gmail.com>
2019-03-05 19:19:35 -0500

    dar estilos con float a las entradas en el archivo front-page.php

--- front-page.php
+++ front-page.php
@@ -3,5 +3,34 @@
 <div id="queVisitar">
 	<?php dynamic_sidebar('front-page'); ?>
 </div>
+<div class="clear"></div>
+<div id="informacionConsejos">
+	<div class="informacion">
+		<?php $informacion = new WP_Query('page_id=12'); ?>
+		<?php while($informacion->have_posts()): $informacion->the_post(); ?>
+			<h2 class="consejos"><?php the_title(); ?></h2>
+			<?php the_excerpt(); //trae el contenido recortado ?>
+		<?php endwhile; wp_reset_postdata(); //para que continue con el otro loop ?>
+	</div>
+	<div class="consejos">
+		<h2 class="consejos">Consejos para viajar a Canada</h2>
+		<?php $args = array(
+			'cat' => 8,
+			'posts_per_page' => 2,
+			'orderby' => 'date',
+			'order' => 'DESC'
+		); ?>
+		<?php $consejos = new WP_Query($args); ?>
+		<?php while($consejos->have_posts()): $consejos->the_post();//con the_post accedemos a los métodos?>
+			<div class="consejoContenido">
+				<a href="<?php the_permalink(); ?>">
+					<?php the_post_thumbnail('guia-toronto'); ?>
+					<h3><?php the_title(); ?></h3>
+				</a>
+				<?php the_excerpt(); //trae el contenido recortado ?>
+			</div>
+		<?php endwhile; wp_reset_postdata(); //para que continue con el otro loop ?>
+	</div>
+</div>
 
 <?php get_footer(); ?>
